97f5c9d722d89101a2f83c42f06c613cae341580
santi-18171 <santimartinez18171920@gmail.com>
2023-09-13 03:08:55 -0300

    Documentación del JS

--- js/script.js
+++ js/script.js
@@ -28,7 +28,11 @@ const strangeArray = [
 function showList(array) {
   const container = document.getElementById("list");
   container.innerHTML = "";
+
+  
   // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+  // Itera a través de los elementos del array y crea elementos de lista (li) para cada uno.
   array.forEach((element) => {
     const li = document.createElement("li");
     li.appendChild(document.createTextNode(element));
@@ -36,9 +40,15 @@ function showList(array) {
   });
 }
 
+// Agrega un evento que se ejecutará cuando el contenido HTML haya sido cargado.
 document.addEventListener("DOMContentLoaded", (e) => {
   // Escribe tu solución aquí
+
+  // Filtra los elementos de strangeArray que son cadenas de texto y los ordena alfabéticamente.
   const filteredArray = strangeArray.filter((element) => typeof element === "string").sort();
+
   // Sugerencia de cómo mostrar el array => showList(strangeArray);
+
+  // Llama a la función showList para mostrar los elementos filtrados en el elemento con id "list".
   showList(filteredArray);
 });
